7eb2f9fd541f36e6630a10ea0da23533e27ba529
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-11-05 20:12:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 const mqtt = require("mqtt");
 
-const host = '172.20.10.4'
+const host = '10.3.141.1'
 const port = '1883'
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 const connectUrl = `mqtt://${host}:${port}`
@@ -36,30 +36,4 @@ client.subscribe(subTopic, () => {
 
 
 
-app.post("/changeStatus", (req, res) => {
-    const changeStatusTopic = "cmnd/tasmota_8231A8/POWER1";
-    client.publish(changeStatusTopic, req.body.status, { qos: 2, retain: false }, (error) => {
-        if (error) {
-            console.error(error)
-        }
-    })
-    res.sendStatus(200)
-});
 
-app.get("/getStatus", (req, res ) => {
-    const getStatusTopic = "cmnd/tasmota_8231A8/Power1"
-    //TODO: sincronizzare
-    client.publish(getStatusTopic, "", { qos: 2, retain: false }, (error) => {
-        if (error) {
-            console.error(error)
-        }
-    })
-    client.once('message', (subTopic, payload) => {
-        console.log('Received Message (ON):', subTopic, payload.toString())
-        status = payload.toString();
-        res.json("status:" + status);
-        res.sendStatus(200);
-    });
-    console.log('status:', status)
-
-});
